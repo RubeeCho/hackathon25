95d2b741cb5911f085b42160ff63a9a7fab4a134
Ruby Cho <rubycho53@gmail.com>
2025-04-11 23:55:16 -0400

    first commit by ruby

---
+++ script.js
@@ -0,0 +1,11 @@
+//get elements from the DOM
+const counterElement = document.getElementById("counter");
+
+let count = 0;//initialize the counter
+count++; //increement
+//update the counter to display the now incremented value
+counterElement.textContent = count; 
+
+
+//if scann failed, update result to "Try again!"
+
